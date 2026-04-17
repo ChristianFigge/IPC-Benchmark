60640167214e11b2914f6e7e113536a4bae88094
ChristianFigge <135016000+ChristianFigge@users.noreply.github.com>
2026-04-17 09:29:41 +0200

    Iteration over multiple predefined message lengths

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use std::io::{Read, Write};
 use std::process::{Command, Stdio};
 use ipc_benchmark::{to_microsecs};
 
-// Predefine IPC message lengths to bench & samples to collect
+// Predefine IPC message lengths to bench & number of samples to collect
 // (maybe as CLI args for main binary?)
-const MSG_SIZES: [usize; 1] = [8]; // Prototype test value
+const MSG_SIZES: [usize; 5] = [
+    8,
+    16 * 1024,
+    64 * 1024,
+    256 * 1024,
+    1024 * 1024,
+];
 const N_SAMPLES: usize = 100;
 
 /** PIPE SPEED BENCH
@@ -21,47 +27,50 @@ fn main() {
     let mut samples : Vec<f64> = Vec::with_capacity(N_SAMPLES);
 
     // TODO iterate over MSG_SIZES...
-    println!("Piping {} * {} Bytes ...", N_SAMPLES, MSG_SIZES[0]);
-    for _ in 0..N_SAMPLES {
-        // Tx takes the msg size as a command line argument
-        let mut tx = Command::new(tx_path.clone())
-            .args(&[MSG_SIZES[0].to_string()])
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("Failed to start Tx");
+    for msg_size in MSG_SIZES.iter() {
+        println!("Piping {} * {} Bytes ...", N_SAMPLES, msg_size);
+        for _ in 0..N_SAMPLES {
+            // Tx takes the msg size as a command line argument
+            let mut tx = Command::new(tx_path.clone())
+                .args(&[msg_size.to_string()])
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("Failed to start Tx");
 
-        let mut rx = Command::new(rx_path.clone())
-            .stdin(tx.stdout.take().unwrap())
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("Failed to start Rx");
+            let mut rx = Command::new(rx_path.clone())
+                .stdin(tx.stdout.take().unwrap())
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("Failed to start Rx");
 
-        // SYNCHRONIZE both processes:
-        // Corrects the timestamps made by Tx/Rx, so the measured duration includes less overhead
-        // 1. Wait for Rx "Ready" Signal
-        let mut rx_out = rx.stdout.take().unwrap();
-        rx_out.read_exact(&mut [0u8; 1])
-            .expect("Failed to get Sync Signal from Rx");
+            // SYNCHRONIZE both processes:
+            // Corrects the timestamps made by Tx/Rx, so the measured duration includes less overhead
+            // 1. Wait for Rx "Ready" Signal
+            let mut rx_out = rx.stdout.take().unwrap();
+            rx_out.read_exact(&mut [0u8; 1])
+                .expect("Failed to get Sync Signal from Rx");
 
-        // 2. Send "GO" Signal to Tx, which waits for it
-        let tx_in = tx.stdin.as_mut().unwrap();
-        tx_in.write(&[1u8])
-            .expect("Failed to send Sync Signal to Tx");
+            // 2. Send "GO" Signal to Tx, which waits for it
+            let tx_in = tx.stdin.as_mut().unwrap();
+            tx_in.write(&[1u8])
+                .expect("Failed to send Sync Signal to Tx");
 
-        // Wait for Rx to return a Sample & Save it in vector
-        rx_out.read_exact(&mut sample_buf)
-            .expect("Failed to read Rx Sample");
-        samples.push(to_microsecs(u64::from_le_bytes(sample_buf)));
+            // Wait for Rx to return a Sample & Save it in vector
+            rx_out.read_exact(&mut sample_buf)
+                .expect("Failed to read Rx Sample");
+            samples.push(to_microsecs(u64::from_le_bytes(sample_buf)));
 
-        // Wait for both processes to finish
-        let _tx_status = tx.wait().unwrap();
-        let _rx_status = rx.wait().unwrap();
+            // Wait for both processes to finish
+            let _tx_status = tx.wait().unwrap();
+            let _rx_status = rx.wait().unwrap();
 
-        // TODO panic if tx_stats != 0 && rx_status != 0
-        //println!("tx exited with: {} \nrx exited with: {}", tx_status.code().unwrap(), rx_status.code().unwrap());
+            // TODO panic if tx_stats != 0 && rx_status != 0
+            //println!("tx exited with: {} \nrx exited with: {}", tx_status.code().unwrap(), rx_status.code().unwrap());
+        }
+
+        eprintln!("Durations in μs: {:?}\n", samples);
+        // TODO log samples somewhere
+        samples.clear();
     }
-
-    eprintln!("Durations in μs: {:?}", samples);
-    // TODO log samples somewhere
 }
