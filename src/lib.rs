800413059cc0b08fe95502bbef4545b7dc278e5c
ChristianFigge <135016000+ChristianFigge@users.noreply.github.com>
2026-07-07 12:43:34 +0200

    TSC Cycles per Nanosecond conversion + Transfer rate results print

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 
 use std::io::{Error, ErrorKind};
 use std::arch::x86_64::__rdtscp;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
 pub const BUF_SIZE: usize = 4096;
 
@@ -21,13 +23,11 @@ pub fn args_error(msg: String) -> Result<(), Error> {
     Err(Error::new(ErrorKind::InvalidInput, msg))
 }
 
-#[inline(always)]
 pub fn mean(v: &[u64]) -> Option<f64> {
     if v.is_empty() { return None }
     Some(v.iter().sum::<u64>() as f64 / (v.len() as f64))
 }
 
-#[inline(always)]
 pub fn stddev(v: &[u64], mean: f64) -> Option<f64> {
     if v.len() < 2 { return None }
     let mut sum = 0.0;
@@ -35,4 +35,19 @@ pub fn stddev(v: &[u64], mean: f64) -> Option<f64> {
         sum += (*val as f64 - mean).powi(2);
     }
     Some( (sum / (v.len()-1) as f64).sqrt() )
+}
+
+pub fn measure_cycles_per_nanosecond() -> f64 {
+    let start_time = Instant::now();
+    let start_cycles = read_tsc();
+    
+    sleep(Duration::from_millis(100));
+    
+    let end_cycles = read_tsc();
+    let end_time = Instant::now();
+    
+    let elapsed_ns = end_time.duration_since(start_time).as_nanos() as f64;
+    let elapsed_cycles = (end_cycles - start_cycles) as f64;
+    
+    elapsed_cycles / elapsed_ns
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ipc_benchmark::{mean, stddev};
+use ipc_benchmark::{mean, stddev, measure_cycles_per_nanosecond};
 use std::io::{Read, Write};
 use std::process::{Command, Stdio};
 
@@ -71,21 +71,6 @@ fn pipe_speed_bench(samples: &mut [u64; N_SAMPLES], msg_size: usize, build_profi
 }
 
 
-fn handle_samples(samples: &[u64]) -> () {
-    // Calculate mean, stddev, coefficient of variation (cv) and relative standard error (rse)
-    let mean = mean(&samples).unwrap();
-    let stddev = stddev(&samples, mean).unwrap();
-    let cv = stddev / mean;
-    let rse = cv / (samples.len() as f64).sqrt();
-
-    eprintln!(
-        "Mean: {mean:.1} cycles, StdDev: {stddev:.1} cycles -> CV = {cv:.3}, RSE = {rse:.3} \n"
-    );
-
-    // TODO log samples somewhere
-}
-
-
 fn hardware_is_supported() -> bool {
     #[cfg(not(target_arch = "x86_64"))]
     { false }
@@ -115,6 +100,27 @@ fn hardware_is_supported() -> bool {
 }
 
 
+fn handle_samples(samples: &[u64], msg_size: usize, cycles_per_nanosecond: f64) -> () {
+    // Calculate mean, stddev, coefficient of variation (cv) and relative standard error (rse)
+    let mean_cycles = mean(&samples).unwrap();
+    let std_cycles = stddev(&samples, mean_cycles).unwrap();
+    let cv = std_cycles / mean_cycles;
+    let rse = cv / (samples.len() as f64).sqrt();
+
+    let mean_ns = mean_cycles / cycles_per_nanosecond;
+    let mean_rate_bit_per_ns = (8 * msg_size) as f64 / mean_ns;
+    let std_ns = std_cycles / cycles_per_nanosecond;
+
+    eprintln!(
+        "Mean: {mean_cycles:.1} cycles ({mean_ns:.1} ns), StdDev: {std_cycles:.1} cycles ({std_ns:.1} ns)\n\
+        -> CV = {cv:.3}, RSE = {rse:.3} \n\
+        -> Mean Transfer Rate: {mean_rate_bit_per_ns:.5} bit/ns \n"
+    );
+
+    // TODO log samples somewhere
+}
+
+
 fn main() {
     // Check hardware support for invariant TSC
     if !hardware_is_supported() {
@@ -123,11 +129,13 @@ fn main() {
 
     // Get current build profile to match the binary paths
     let build_profile = if cfg!(debug_assertions) { "debug" } else { "release" };
-    let mut samples= [0u64; N_SAMPLES];
+    let cycles_per_nanosecond = measure_cycles_per_nanosecond();
 
+    // Sample buffer on stack
+    let mut samples= [0u64; N_SAMPLES];
     // BENCHMARK PIPE SPEED
-    for msg_size in MSG_SIZES.iter() {
-        pipe_speed_bench(&mut samples, *msg_size, build_profile);
-        handle_samples(&samples);
+    for msg_size in MSG_SIZES {
+        pipe_speed_bench(&mut samples, msg_size, build_profile);
+        handle_samples(&samples, msg_size, cycles_per_nanosecond);
     }
 }
